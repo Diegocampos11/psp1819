5be660a96673700b03ae4d7fb43a0b7cbb492399
Diego Campos <244822@iessierradeguara.com>
2018-10-10 13:07:27 +0200

    w
    w

--- proyectoVisual/tuberias/escritor.c
+++ proyectoVisual/tuberias/escritor.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    
+    char* frase = "Hola soy el escritor";
+    int descriptorEscritura;
+    descriptorEscritura = open( "./tuberia", O_WRONLY );
+    write( descriptorEscritura, frase, strlen( frase ) * sizeof(char) );
+    close( descriptorEscritura );
     return 0;
 }
--- proyectoVisual/tuberias/lector.c
+++ proyectoVisual/tuberias/lector.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
-    
+    char leido[100];
+    int desLectura;
+    desLectura = open( "./tuberia", O_RDONLY );
+    read( desLectura, leido, 100 * sizeof(char) );
+    fprintf( stdout, "%s\n", leido );
+    close( desLectura );
     return 0;
 }
